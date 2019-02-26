cf9a8ce0b5127af7f04056e06f63056d5fedf055
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-02-26 23:38:02 +0300

    Edit HelloWorld.

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -11,8 +11,9 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
   * Author: UC San Diego Coursera Intermediate Programming team
-  * @author 
-  * Date: July 17, 2015
+  * @author eglushkov
+  * @version 1.0
+  * Date: February 26, 2019
   * */
 public class HelloWorld extends PApplet
 {
@@ -36,7 +37,7 @@ public class HelloWorld extends PApplet
 	UnfoldingMap map2;
 
 	public void setup() {
-		size(800, 600, P2D);  // Set up the Applet window to be 800x600
+		size(1000, 800, P2D);  // Set up the Applet window to be 800x600
 		                      // The OPENGL argument indicates to use the 
 		                      // Processing library's 2D drawing
 		                      // You'll learn more about processing in Module 3
@@ -65,7 +66,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 25, 50, 450, 700, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
@@ -76,6 +77,11 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 525, 50, 450, 700, provider);
+		
+		map2.zoomAndPanTo(zoomLevel, new Location(44.6f, 33.51f));
+		
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
@@ -83,7 +89,9 @@ public class HelloWorld extends PApplet
 	public void draw() {
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
-		map1.draw();
+		map1.draw();		
+
+		map2.draw();
 	}
 
 	
